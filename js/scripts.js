201b609440416983a417e2359949c4e6b2c96f23
Dale S Hendrickson <dale@dshendrickson.com>
2016-09-08 13:32:32 -0600

    Idea save button capturing value from input fields and writing to the list

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 $ideaTitleInput = $('#ideaTitleInput');
 $ideaBodyInput = $('#ideaBodyInput');
 $ideaSaveButton = $('#ideaSaveButton');
+$ideaList = $('#ideaList');
 
 // disable submit button if title or body are missing
 $('#ideaTitleInput, #ideaBodyInput').on('keyup', function() {
@@ -13,12 +14,21 @@ $('#ideaTitleInput, #ideaBodyInput').on('keyup', function() {
 
 $ideaSaveButton.on('click', function() {
   console.log('ping');
+  var idea = createIdea( $ideaTitleInput.val(), $ideaBodyInput.val() );
+  $ideaList.prepend(idea)
 });
 
 function ping() {
   console.log('ping');
 };
 
+function createIdea(ideaTitle, ideaBody) {
+  return $(
+    '<article class="idea"><h3 class="ideaTitle">' + ideaTitle +
+    '</h3><div class="deleteIdeaButton"></div><p class="ideaBody">' + ideaBody +
+    '</p><div class="ideaQualityContainer"><div class="ideaPromoteButton"></div><div class="ideaDemoteButton"></div><p class="ideaQuality"><span class="ideaQualityLabel">quality: </span><span class="ideaQualityValue">swill</span></p></div></article>'
+  );
+};
 
 
 
