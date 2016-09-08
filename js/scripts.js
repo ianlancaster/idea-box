b90153850af7cb4ed305388e203b8687db1163f4
Ian Lancaster <ianclancaster@gmail.com>
2016-09-08 13:03:50 -0600

    added t to javascript, some css tweaks, started working on ideaSaveButton javascript

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+$ideaTitleInput = $('#ideaTitleInput');
+$ideaBodyInput = $('#ideaBodyInput');
+$ideaSaveButton = $('#ideaSaveButton');
+
+// disable submit button if title or body are missing
+$('#ideaTitleInput, #ideaBodyInput').on('keyup', function() {
+  if($ideaTitleInput == '' || $ideaBodyInput == '') {
+    $ideaSaveButton.prop('disabled', true);
+  } else {
+    $ideaSaveButton.prop('disabled', false);
+  }
+});
+
+$ideaSaveButton.on('click', function() {
+  console.log('ping');
+});
+
+function ping() {
+  console.log('ping');
+};
+
+
+
+
+
+// Unassigned
+// function Idea(id,title,body,quality) {
+//   this.id = id;
+//   this.title = title;
+//   this.body = body;
+//   this.quality = quality;
+// };
+//
