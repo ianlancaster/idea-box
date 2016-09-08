2f33ba30ae9bf9ecb5baa8cc93b73ec62e8f44d1
Dale S Hendrickson <dale@dshendrickson.com>
2016-09-08 16:41:11 -0600

    Delete functionality attempt but ideaIndex is being trucated to 1 entry

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,7 @@ var $ideaList = $('#ideaList');
 var ideaIndex = [];
 
 checkIdeaIndex();
+getIdeas();
 
 // disable submit button if title or body are missing
 $('#ideaTitleInput, #ideaBodyInput').on('keyup', function() {
@@ -19,48 +20,49 @@ function setSaveButtonStatus () {
   };
 };
 
- $ideaSaveButton.on('click', function() {
+$ideaSaveButton.on('click', function() {
   console.log('ping');
   var id = Date.now();
   var quality = 'swill';
   var title = $ideaTitleInput.val();
   var body = $ideaBodyInput.val();
-
   ideaIndex.push(id);
   localStorage.setItem('ideaIndex',JSON.stringify(ideaIndex));
-
   var idea = {
     id: id,
     quality: quality,
     title: title,
     body: body
   }
-
-
   var stringifiedIdea = JSON.stringify(idea);
   localStorage.setItem(id,stringifiedIdea);
-  var gotIdea= localStorage.getItem(id);
+  var gotIdea = localStorage.getItem(id);
   var parsedIdea = JSON.parse(gotIdea);
-  var compiledIdea = createIdea(parsedIdea.title, parsedIdea.body, parsedIdea.quality);
+  var compiledIdea = createIdea(parsedIdea.title, parsedIdea.body, parsedIdea.quality, parsedIdea.id);
   $ideaList.prepend(compiledIdea);
-
   clearInputFields();
   setSaveButtonStatus();
 });
 
+$(document).on('click', '.deleteIdeaButton', function () {
+  console.log($(this).parent().prop('id'));
+  var thisId = $(this).parent().prop('id');
+  localStorage.removeItem(thisId);
+  localStorage.setItem('ideaIndex',JSON.stringify(ideaIndex.splice( $.inArray(thisId, ideaIndex), 1 )));
+  checkIdeaIndex();
+  getIdeas();
+});
+
 function getIdeas() {
-
-  // var ideaIndex[] =
-  //This will need to run on page load:
-  //if ideaIndex array exists in localStorage, get the array
-  //else create and empty array to prevent an error from the push to ideaIndex array then push to localStorage
-
-  var gotIdea= localStorage.getItem(id);
-  var parsedIdea = JSON.parse(gotIdea);
-  var compiledIdea = createIdea(parsedIdea.title, parsedIdea.body, parsedIdea.quality);
-  $ideaList.prepend(compiledIdea);
+  var numberOfIdeas = ideaIndex.length;
+  for (var i = 0; i < ideaIndex.length; i++) {
+    var id = ideaIndex[i];
+    var gotIdea = localStorage.getItem(id);
+    var parsedIdea = JSON.parse(gotIdea);
+    var compiledIdea = createIdea(parsedIdea.title, parsedIdea.body, parsedIdea.quality, parsedIdea.id);
+    $ideaList.prepend(compiledIdea);
+  };
 };
-
 
 function checkIdeaIndex() {
   if (localStorage.getItem('ideaIndex') === null) {
@@ -70,21 +72,14 @@ function checkIdeaIndex() {
   }
 };
 
-// $ideaSaveButton.on('click', function() {
-//   console.log('ping');
-//   var idea = createIdea( $ideaTitleInput.val(), $ideaBodyInput.val() );
-//   $ideaList.prepend(idea);
-//   clearInputFields();
-//   setSaveButtonStatus();
-// });
 
 function ping() {
   console.log('ping');
 };
 
-function createIdea(ideaTitle, ideaBody, ideaQuality) {
+function createIdea(ideaTitle, ideaBody, ideaQuality, ideaId) {
   return $(
-    '<article class="idea"><h3 class="ideaTitle">' + ideaTitle +
+    '<article id="' + ideaId + '" class="idea"><h3 class="ideaTitle">' + ideaTitle +
     '</h3><div class="deleteIdeaButton"></div><p class="ideaBody">' + ideaBody +
     '</p><div class="ideaQualityContainer"><div class="ideaPromoteButton"></div><div class="ideaDemoteButton"></div><p class="ideaQuality"><span class="ideaQualityLabel">quality: </span><span class="ideaQualityValue">' + ideaQuality + '</span></p></div></article>'
   );
