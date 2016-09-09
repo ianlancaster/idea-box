8461386d323e46897b6af19de1671c454165e7fd
Dale S Hendrickson <dale@dshendrickson.com>
2016-09-09 15:30:43 -0600

    Remove functionality of an Idea from the ideaList

--- js/scripts.js
+++ js/scripts.js
@@ -12,6 +12,11 @@ $ideaSaveButton.on('click', function() {
   ideaList.add();
 });
 
+$(document).on('click', '.delete-idea-button', function () {
+  var thisId = $(this).parent().prop('id');
+  ideaList.remove(thisId);
+});
+
 function returnIdeaTitle () {
   return $ideaTitleInput.val();
 };
@@ -33,13 +38,27 @@ var ideaList = {
     this.ideas.push(new Idea(returnIdeaTitle(), returnIdeaBody()));
     this.store();
   },
-  remove: function () {},
+  remove: function (id) {
+    id = parseInt(id);
+    console.log(this.ideas);
+    this.ideas = this.ideas.filter(function (idea) {
+      return idea.id !== id;
+    });
+    this.store();
+  },
   find: function () {},
   store: function () {
     localStorage.setItem('ideas', JSON.stringify(this.ideas));
     this.render();
   },
-  retreive: function () {},
+  retreive: function () {
+    var retreivedIdeas = JSON.parse(localStorage.getItem('ideas'));
+    if (retreivedIdeas) {
+      this.ideas = retreivedIdeas.map(function (idea){
+        return new Idea(idea.title, idea.body, idea.quality, idea.id);
+      });
+    }
+  },
   render: function () {
     $ideaList.html(this.ideas.map(function (idea) {
       return idea.toHTML()
@@ -62,3 +81,6 @@ Idea.prototype = {
     return $('<article id="' + this.id + '" class="idea"><h3 class="idea-title">' + this.title + '</h3><div class="delete-idea-button"></div><p class="idea-body">' + this.body + '</p><div class="idea-quality-container"><div class="idea-promote-button"></div><div class="idea-demote-button"></div><p class="ideaQuality"><span class="idea-quality-label">quality: </span><span class="idea-quality-value">' + this.quality + '</span></p></div></article>');
   }
 }
+
+ideaList.retreive();
+ideaList.render();
