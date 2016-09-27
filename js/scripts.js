53249e90b1038d8e4894881e8413684c365596f1
Ian Lancaster <ianclancaster@gmail.com>
2016-09-27 01:03:03 -0600

    Linted javascript and html files

--- js/scripts.js
+++ js/scripts.js
@@ -48,16 +48,16 @@ $(document).on('keyup', '#idea-search-input', function(){
   } else {
     ideaList.retreive();
     ideaList.render();
-  };
+  }
 });
 
 function returnIdeaTitle () {
   return $ideaTitleInput.val();
-};
+}
 
 function returnIdeaBody () {
   return $ideaBodyInput.val();
-};
+}
 
 function setSaveButtonStatus () {
   var enableDisableButton = ($ideaTitleInput.val() === '' || $ideaBodyInput.val() === '');
@@ -92,10 +92,10 @@ var ideaList = {
     this.ideas.forEach(function(idea) {
       if (idea.title.indexOf(searchValue) > -1) {
         matches.push(idea);
-      };
+      }
       if (idea.body.indexOf(searchValue) > -1 && idea.body.indexOf(searchValue) !== idea.title.indexOf(searchValue)) {
         matches.push(idea);
-      };
+      }
     });
     var sortedIdeasId = matches.sort(function(a, b) {
       return b.id-a.id;
@@ -118,10 +118,10 @@ var ideaList = {
   render: function () {
       // Ref: http://www.w3schools.com/jsref/jsref_sort.asp
       var sortedIdeasId = this.ideas.sort(function(a, b) {
-        return b.id-a.id
+        return b.id-a.id;
       });
       $ideaList.html(sortedIdeasId.map(function (idea) {
-        return idea.toHTML()
+        return idea.toHTML();
       }));
     }
 };
@@ -131,7 +131,7 @@ function Idea(title, body, quality, id) {
   this.body = body;
   this.quality = quality || 'swill';
   this.id = id || Date.now();
-};
+}
 
 Idea.prototype = {
   qualityUp: function () {
@@ -151,22 +151,14 @@ Idea.prototype = {
       this.quality = 'swill';
     }
     if (this.quality === 'genius') {
-      this.quality = 'plausible'
+      this.quality = 'plausible';
     }
     ideaList.store();
     ideaList.search(searchValue);
   },
   toHTML: function () {
     return $(
-      '<article id="'
-      + this.id +
-      '" class="idea"><input type="text" class="idea-title" value="'
-      + this.title +
-      '"><div class="delete-idea-button"></div><input type="text" class="idea-body" value="'
-      + this.body +
-      '"><div class="idea-promote-button"></div><div class="idea-demote-button"></div><p class="idea-quality"><span class="idea-quality-label">quality: </span><span class="idea-quality-value">'
-      + this.quality +
-      '</span></p></article>');
+      '<article id="' + this.id + '" class="idea"><input type="text" class="idea-title" value="' + this.title + '"><div class="delete-idea-button"></div><input type="text" class="idea-body" value="' + this.body + '"><div class="idea-promote-button"></div><div class="idea-demote-button"></div><p class="idea-quality"><span class="idea-quality-label">quality: </span><span class="idea-quality-value">' + this.quality + '</span></p></article>');
   },
   updateBody: function(body) {
     this.body = body;
@@ -176,7 +168,7 @@ Idea.prototype = {
     this.title = title;
     ideaList.store();
   }
-}
+};
 
 ideaList.retreive();
 ideaList.render();
