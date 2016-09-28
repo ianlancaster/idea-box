a679237ec58a6a87cbf9015408170ea50a0beb39
Ian Lancaster <ianclancaster@gmail.com>
2016-09-27 22:27:41 -0600

    functioning non-refactored mvp

--- main.bundle.js
+++ main.bundle.js
@@ -42,15 +42,54 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	__webpack_require__(1);
+	(function webpackMissingModule() { throw new Error("Cannot find module \"app1.js\""); }());
+
+
+/***/ },
+/* 1 */
+/***/ function(module, exports, __webpack_require__) {
+
+	var scripts = __webpack_require__(2);
+
+/***/ },
+/* 2 */
 /***/ function(module, exports) {
 
-	'use strict';
+	var ideaTitleInput = document.querySelector('#idea-title-input');
+	var ideaBodyInput = document.querySelector('#idea-body-input');
+	var saveButton = document.querySelector('#idea-save-button');
+	var ideaList = document.querySelector('#idea-list');
 
-	var sayHello = function sayHello() {
-	  return console.log('Hello');
-	};
+	saveButton.addEventListener('click', function (e) {
+	  e.preventDefault();
+	  var title = ideaTitleInput.value;
+	  var body = ideaBodyInput.value;
+	  addElement(title, body);
+	});
 
-	sayHello();
+	function addElement(title, body, quality) {
+	  var title = title || 'Example Idea';
+	  var body = body || 'Default idea description';
+	  var quality = quality || 'swill';
+	  var idea = document.createElement('article');
+	  idea.className = 'idea';
+	  idea.innerHTML = `
+	    <h3 class="idea-title">${ title }</h3>
+	    <div class="delete-idea-button"></div>
+	    <p class="idea-body">${ body }</p>
+	    <div class="idea-quality-container">
+	      <div class="idea-promote-button"></div>
+	      <div class="idea-demote-button"></div>
+	      <p class="idea-quality">
+	        <span class="idea-quality-label">quality: </span><span class="idea-quality-value">${ quality }</span>
+	      </p>
+	    </div>
+	  `;
+	  ideaList.appendChild(idea);
+	}
 
 /***/ }
 /******/ ]);
